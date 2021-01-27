db141c39d828042e4eccd39741f52de0686d5c86
chuchu-z <1813713320@qq.com>
2021-01-27 12:02:23 +0800

    git bash 主题

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # local-config
 my windows localhost config
+.
+|-- README.md
+|-- doker_bashrc  //docker容器配置文件
+|-- git-prompt.sh //git bash 主题配置
+|-- git-prompt.sh_changeTitle //git bash 修改标题
+|-- gitUpdateUserInfo.sh //修改git 提交作者名称
+|-- phpcc //phpcc脚本
+|-- phpcc.ini //phpcc配置
+|-- postManToMarkdown.php //postman导出json转换markdown
+|-- pre-commit //git 提交前钩子
+|-- profile //etc下的配置文件
+|-- push.sh //一键提交脚本
+|-- resetRedisCache.php //重置redis的key
+|-- vimrc //vim编辑器配置
+`-- windows.sh //windows 10 创建软链接
---
+++ windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#cd ./workspace/www
+
+# 在 windows 下的 Git bash 使用 ln 命令 会变成 cp 的效果
+# windows系统创建软链接方法: mklink /d "需要创建链接文件的绝对路径" "指向文件的绝对路径" cmd下管理员身份运行
+# 下面的路径记得改成你的项目路径
+
+#rm -rf aex.com
+mklink /d D:\MyPHP\WWW\project\workspace\www\aex.com D:\MyPHP\WWW\project\workspace\www\svr
+
+#cd svr
+#rm  -rf page
+mklink /d D:\MyPHP\WWW\project\workspace\www\svr\page D:\MyPHP\WWW\project\workspace\www\page
+#rm -rf admin
+mklink /d D:\MyPHP\WWW\project\workspace\www\svr\admin D:\MyPHP\WWW\project\workspace\www\newadmin\bitcc_administrator
