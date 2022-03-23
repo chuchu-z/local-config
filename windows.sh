b84410e00ed9d52a28a410929ce445dfcd1077a3
chuchu-z <1813713320@qq.com>
2022-03-23 14:50:05 +0800

    增加注释

--- windows.sh
+++ windows.sh
@@ -4,6 +4,8 @@
 # 在 windows 下的 Git bash 使用 ln 命令 会变成 cp 的效果
 # windows系统创建软链接方法: mklink /d "需要创建链接文件的绝对路径" "指向文件的绝对路径" cmd下管理员身份运行
 # 下面的路径记得改成你的项目路径
+# 删除软链接使用 rmdir
+# 例: rmdir D:\MyPHP\WWW\project\workspace\www\aex.com
 
 #rm -rf aex.com
 mklink /d D:\MyPHP\WWW\project\workspace\www\aex.com D:\MyPHP\WWW\project\workspace\www\svr
